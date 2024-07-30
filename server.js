41619c94d52cbb7a091044664206681d5fad6321
Elijah <elijahab716@gmail.com>
2024-07-29 21:14:47 -0500

    shuffling and sliding

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const uuid = require('uuid'); 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
